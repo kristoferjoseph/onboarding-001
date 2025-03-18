ddfd98786b4ef0df1dfd9c8ca7f73943daea5f46
Kristofer Joseph <kristofer.joseph@sanity.io>
2025-03-18 13:24:05 -0700

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  __experimental_appConfiguration: {
+    organizationId: 'oMwEnnWOs',
+    appLocation: './src/App.tsx',
+  },
+})
